3b6e0c2dae48708b75fc30dab43d06f97e170ace
袁华 <lasyer@foxmail.com>
2018-07-11 16:37:16 +0800

    修正

--- src/test/java/com/test/service/ArticleTest.java
+++ src/test/java/com/test/service/ArticleTest.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
  * @author Lasy
  * @date 2018/6/22 11:18
  */
+
 public class ArticleTest extends BasicTest {
     @Autowired
     private ArticleService service;
@@ -17,10 +18,11 @@ public class ArticleTest extends BasicTest {
     @Test
     public void test() {
         ArticleParam param = new ArticleParam();
-        param.setTitle("阿根廷0:3惨败克罗地亚");
-        param.setContent("梅西令人失望");
+        param.setTitle("某主角悲惨人生表示不再做人");
+        param.setContent("jojo,我不做人啦!");
         param.setUserId(15);
         param.setAuthor("匿名");
-        service.create(param);
+        Integer i = service.create(param);
+        System.out.println(i);
     }
 }
